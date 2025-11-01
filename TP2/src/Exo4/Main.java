0f61b3695cbe6f2f7dacd6bcf215a79cf86b37ae
marouane-nouara <nouaramarouane@gmail.com>
2025-11-01 12:53:11 +0100

    Main.java

--- TP2/src/Exo4/Main.java
+++ TP2/src/Exo4/Main.java
@@ -2,9 +2,39 @@
 package Exo4;
 
 
+package Exo4;
+
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-    Etudiant E1 = new Etudiant(001,"Tran","Binh Minh", 21,"BDML", 17.00);
-    System.out.println(E1);
+        Etudiant e1 = new Etudiant(1, "Tran", "Binh Minh", 21, "BDML", 17.0);
+        Etudiant e2 = new Etudiant(2, "Dupont", "Alice", 19, "Informatique", 14.5);
+        Etudiant e3 = new Etudiant(3, "Martin", "Yassine", 21, "Maths", 13.0);
+        Etudiant e4 = new Etudiant(4, "Durand", "Lina", 20, "BDML", 15.8);
+
+        // Groupe
+        Groupe g = new Groupe("BDML-1");
+        g.ajouterEtudiant(e1);
+        g.ajouterEtudiant(e2);
+        g.ajouterEtudiant(e3);
+        g.ajouterEtudiant(e4);
+
+        // 1) Affichage
+        g.afficherEtudiants();
+
+        // 2) Tri par âge 
+        g.trierParAge();
+        System.out.println("\nAprès tri par âge :");
+        g.afficherEtudiants();
+
+        // 3) Recherche spécialité par numéro
+        String spec = g.rechercherSpecialiteParNumero(3);
+        System.out.println("\nSpécialité de l'étudiant n°3 : " + spec);
+
+        // 4) Prénoms des étudiants de 21 ans
+        List<String> prenoms21 = g.prenomsParAge(21);
+        System.out.println("\nPrénoms des étudiants de 21 ans : " + prenoms21);
+    }
 }
-}
+
